f5d1ed2fb1a7edb49c8806c46d8923f1e8daeb20
Verron Knowles <Verron.Knowles@develme.com>
2022-07-13 19:54:37 -0400

    Adjust test names

--- cypress/e2e/exercise-0.cy.ts
+++ cypress/e2e/exercise-0.cy.ts
@@ -1,7 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("Exercise 0", () => {
-  it("visits the app root", () => { // @TODO Update when landing page is created
+  it("Visits home page", () => { // @TODO Update when landing page is created
     cy.visit("/");
     cy.contains("h1", "Welcome!");
   });
