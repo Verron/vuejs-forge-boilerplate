fae8cd4eb0d7379da1a05327e06141cbc606ce5f
Verron Knowles <Verron.Knowles@develme.com>
2022-07-13 20:57:22 -0400

    Adjustments to exercise 5

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@ import { ref, computed } from 'vue';
 import { Drawer, DrawerContent } from "@progress/kendo-vue-layout";
 import { useRouter, type RouteLocationRaw } from 'vue-router';
 import { useLocalStorage } from "@vueuse/core";
-import Alerts from "./components/Alerts.vue";
 
 
 const showTeamDropdown = ref(false);
@@ -95,5 +94,5 @@ const toggleTeams = () => {
       </DrawerContent>
     </Drawer>
   </div>
-  <Alerts></Alerts>
+  <alerts></alerts>
 </template>
